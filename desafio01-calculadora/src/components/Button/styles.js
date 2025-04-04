c4ba66547dee68c2a66ecd359d7a759d766d22d1
Thiago Zani <thiagozani@siatt.com.br>
2025-04-04 16:03:06 -0300

    feat: inicio projeto calculadora em Reast

---
+++ desafio01-calculadora/src/components/Button/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+padding: 20px;
+ border: 1px solid #CDCDCD;
+ background-color: #00AAF0;
+ color: #fff;
+ font-size: 24px;
+ font-weight: 700;
+ flex: 1;
+`;
---
+++ desafio01-calculadora/src/components/Input/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 70px;
+  background-color: #aaffaa;
+
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  font-size: 24px;
+  font-family: "Roboto", sans-serif;
+
+  input {
+    width: 100%;
+    height: 70px;
+    background-color: #aaffaa;
+    border: 0;
+    flex-direction: column;
+    align-items: flex-end;
+    padding: 0 20px;
+    font-size: 24px;
+    font-family: "Roboto", sans-serif;
+    color: #000;
+    text-align: right;
+  }
+`;
